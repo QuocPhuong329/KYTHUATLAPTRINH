d25e3c48f9dd1743c667b5fefa836b47602571d1
QuocPhuong329 <phuong107435@donga.edu.vn>
2024-11-06 10:51:25 +0700

    Add files via upload

---
+++ BaiTap1.java
@@ -0,0 +1,31 @@
+package buoi5;
+
+public class BaiTap1 {
+
+
+    public static void main(String[] args) {
+        
+    int a =0,b,c;
+    ++a;
+    b =a;
+    c = ++a;
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    }
+
+
+    
+    
+
+public static void viDu4() {
+    int a =0,b, c;
+    ++a;
+    b =++a;
+    c =++a;
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+}
+}
+
